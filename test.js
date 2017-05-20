e182f87934c3a2c64fa9a58ab9905fef9f6877d8
stephentuso <tusodev@gmail.com>
2017-05-20 17:05:20 -0400

    Move helper functions to bottom

--- test.js
+++ test.js
@@ -18,90 +18,7 @@ const filled = fillColors({}, ['red', 'green', 'blue'])
 
 let testIndex = 0
 
-function copyObj (obj) {
-  return JSON.parse(JSON.stringify(obj))
-}
-
-function copyArrVals (arr) {
-  let copy = []
-  for (let i = 0; i < arr.length; i++) {
-    copy[i] = arr[i]
-  }
-  return copy
-}
-
-function makeArr (length, value) {
-  value = value || 0
-  let arr = []
-  for (let i = 0; i < length; i++) {
-    arr[i] = value
-  }
-  return arr
-}
-
-function fillColors (data, colors, length, value) {
-  length = length || LENGTH
-  value = value || 1
-  let obj = copyObj(data)
-  colors.forEach((color) => {
-    obj[color] = makeArr(length, value)
-  })
-  return obj
-}
-
-function outputCanvasImage (stream, filename) {
-  let dir = path.join(__dirname, 'tmp')
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir)
-  let out = fs.createWriteStream(path.join(dir, filename))
-  stream.on('data', chunk => {
-    out.write(chunk)
-  })
-  stream.on('end', console.log)
-}
-
-/**
- *
- * @param t ava test
- * @param {CanvasPixelArray | Array} imageData  canvas pixel data
- * @param {string} onChannels Channels required to match onVal (e.g. 'rgb')
- * @param {string} [offChannels] Channels required to match offVal
- * @param {number} [onVal] default 255
- * @param {number} [offVal] default 0
- */
-function testChannels (t, imageData, onChannels, offChannels, onVal, offVal) {
-  onVal = onVal || 255
-  offVal = offVal || 0
-  let channels = {
-    r: null,
-    g: null,
-    b: null,
-    a: null
-  }
-  for (let channel in channels) {
-    if (!channels.hasOwnProperty(channel)) continue
-    let regex = new RegExp(channel)
-    if (onChannels.match(regex)) {
-      channels[channel] = 1
-    } else if (offChannels.match(regex)) {
-      channels[channel] = 0
-    }
-  }
-  for (let i = 0; i < imageData.length; i += 4) {
-    for (let channel in channels) {
-      if (!channels.hasOwnProperty(channel)) continue
-      let offset = 'rgba'.indexOf(channel)
-      if (channels[channel] === 1) {
-        t.true(imageData[i + offset] === onVal)
-      } else if (channels[channel] === 0) {
-        t.true(imageData[i + offset] === offVal)
-      }
-    }
-  }
-}
-
-function getFullImageDataArray (canvas) {
-  return canvas.getContext('2d').getImageData(0, 0, CANV_WIDTH, CANV_HEIGHT).data
-}
+// -- Tests -- //
 
 test.beforeEach(t => {
   t.context.canvas = new Canvas(CANV_WIDTH, CANV_HEIGHT)
@@ -211,3 +128,90 @@ test('half red, half green', t => {
   testChannels(t, leftHalf, 'ra', 'gb')
   testChannels(t, rightHalf, 'ga', 'rb')
 })
+
+// -- Helper Functions -- //
+
+function copyObj (obj) {
+  return JSON.parse(JSON.stringify(obj))
+}
+
+function copyArrVals (arr) {
+  let copy = []
+  for (let i = 0; i < arr.length; i++) {
+    copy[i] = arr[i]
+  }
+  return copy
+}
+
+function makeArr (length, value) {
+  value = value || 0
+  let arr = []
+  for (let i = 0; i < length; i++) {
+    arr[i] = value
+  }
+  return arr
+}
+
+function fillColors (data, colors, length, value) {
+  length = length || LENGTH
+  value = value || 1
+  let obj = copyObj(data)
+  colors.forEach((color) => {
+    obj[color] = makeArr(length, value)
+  })
+  return obj
+}
+
+function outputCanvasImage (stream, filename) {
+  let dir = path.join(__dirname, 'tmp')
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir)
+  let out = fs.createWriteStream(path.join(dir, filename))
+  stream.on('data', chunk => {
+    out.write(chunk)
+  })
+  stream.on('end', console.log)
+}
+
+/**
+ *
+ * @param t ava test
+ * @param {CanvasPixelArray | Array} imageData  canvas pixel data
+ * @param {string} onChannels Channels required to match onVal (e.g. 'rgb')
+ * @param {string} [offChannels] Channels required to match offVal
+ * @param {number} [onVal] default 255
+ * @param {number} [offVal] default 0
+ */
+function testChannels (t, imageData, onChannels, offChannels, onVal, offVal) {
+  onVal = onVal || 255
+  offVal = offVal || 0
+  let channels = {
+    r: null,
+    g: null,
+    b: null,
+    a: null
+  }
+  for (let channel in channels) {
+    if (!channels.hasOwnProperty(channel)) continue
+    let regex = new RegExp(channel)
+    if (onChannels.match(regex)) {
+      channels[channel] = 1
+    } else if (offChannels.match(regex)) {
+      channels[channel] = 0
+    }
+  }
+  for (let i = 0; i < imageData.length; i += 4) {
+    for (let channel in channels) {
+      if (!channels.hasOwnProperty(channel)) continue
+      let offset = 'rgba'.indexOf(channel)
+      if (channels[channel] === 1) {
+        t.true(imageData[i + offset] === onVal)
+      } else if (channels[channel] === 0) {
+        t.true(imageData[i + offset] === offVal)
+      }
+    }
+  }
+}
+
+function getFullImageDataArray (canvas) {
+  return canvas.getContext('2d').getImageData(0, 0, CANV_WIDTH, CANV_HEIGHT).data
+}
